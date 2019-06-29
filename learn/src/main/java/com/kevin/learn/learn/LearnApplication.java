ce59ce5b0a0a562fcdaa10706458a0ba6d3a0e0c
kevin <hewenkai_1992@sina.com>
2019-06-29 23:03:29 +0800

    新建学习项目第一次提交

---
+++ learn/src/main/java/com/kevin/learn/learn/LearnApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.learn.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnApplication.class, args);
+    }
+
+}
