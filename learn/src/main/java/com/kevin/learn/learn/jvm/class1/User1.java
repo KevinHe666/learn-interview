5e4b2cd023d031c71b8248b96f13ce2c3bb69b7d
kevin <hewenkai_1992@sina.com>
2019-07-01 18:16:08 +0800

    新加测试类

---
+++ learn/src/main/java/com/kevin/learn/learn/jvm/class1/User1.java
@@ -0,0 +1,18 @@
+package com.kevin.learn.learn.jvm.class1;
+
+/**
+ * @Description:
+ * @Author: Kevin
+ * @Create 2019-07-01 09:39
+ */
+public class User1 {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ learn/src/main/java/com/kevin/learn/learn/jvm/class1/test1.java
@@ -0,0 +1,10 @@
+package com.kevin.learn.learn.jvm.class1;
+
+/**
+ * @Description:
+ * @Author: Kevin
+ * @Create 2019-07-01 09:38
+ */
+public class test1 {
+
+}
