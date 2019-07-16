beeea153a4de2a324f4b89d035ad6ad6fe583a9d
kevin <hewenkai_1992@sina.com>
2019-07-16 18:29:28 +0800

    稍微修改下代码

--- learn/src/main/java/com/kevin/learn/learn/LearnApplication.java
+++ learn/src/main/java/com/kevin/learn/learn/LearnApplication.java
@@ -2,6 +2,10 @@ package com.kevin.learn.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * @Description:
@@ -13,6 +17,16 @@ public class LearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("1024000KB");
+        return factory.createMultipartConfig();
     }
 
     private static Object lock1 = new Object();
