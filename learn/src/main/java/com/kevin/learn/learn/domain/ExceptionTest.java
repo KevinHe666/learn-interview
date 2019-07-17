00ada7dbee7109962a72335279bbbc7953b116be
kevin <hewenkai_1992@sina.com>
2019-07-17 18:16:20 +0800

    处理异常

--- learn/src/main/java/com/kevin/learn/learn/domain/ExceptionTest.java
+++ learn/src/main/java/com/kevin/learn/learn/domain/ExceptionTest.java
@@ -31,9 +31,14 @@ public class ExceptionTest {
      */
     @ExceptionHandler(value = MyException.class)
     Object handleMyException(Exception e) {
+        //进行页面跳转
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("error.html");
         modelAndView.addObject("msg", e.getMessage());
         return modelAndView;
+        //也可以返回json数据 由前端去判断
+//        Map<String, String> map = new HashMap<>();
+//        map.put("status", "-1");
+//        return map;
     }
 }
