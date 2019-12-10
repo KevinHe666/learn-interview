d92dbd316a4492f8e8330ad181a1a07bed78ccd7
kevin <hewenkai_1992@sina.com>
2019-12-10 18:17:24 +0800

    新学习东西提交

--- learn/src/main/java/com/kevin/learn/learn/leetcode/LengthOfLongestSubstring.java
+++ learn/src/main/java/com/kevin/learn/learn/leetcode/LengthOfLongestSubstring.java
@@ -1,5 +1,8 @@
 package com.kevin.learn.learn.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description:给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。 示例 1:
  * <p>
@@ -14,4 +17,18 @@ package com.kevin.learn.learn.leetcode;
  * @Create 2019-12-10 18:06
  */
 public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
 }
